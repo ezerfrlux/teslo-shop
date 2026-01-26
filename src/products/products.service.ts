729eef44b82e4fc49d85204b42eb2a5b823829eb
ezerfrlux <aaron.salinas318@gmail.com>
2026-01-26 10:13:50 -0600

    feat: update ProductsService to handle both UUID and slug for product retrieval; add uuid dependency

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -11,6 +11,8 @@ import { Product } from './entities/product.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import {validate as isUUID} from "uuid"
+
 
 @Injectable()
 export class ProductsService {
@@ -39,10 +41,18 @@ export class ProductsService {
     });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id });
+  async findOne(term: string) {
+    let product:Product | null
+
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({id: term})
+    } else {
+      product = await this.productRepository.findOneBy({slug: term})
+    }
+
+    // const product = await this.productRepository.findOneBy({ term });
     if (!product)
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new NotFoundException(`Product with with ${term} not found`);
 
     return product;
   }
